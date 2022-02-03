ab70d1085b5754416db7b7bac079e9c9a0ff7dd1
Luiz Souza <luiz.souz404@gmail.com>
2022-02-03 18:31:30 -0300

    feat: Partner section into main page

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: "es5",
+	tabWidth: 1,
+	useTabs: false,
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	tsxSingleQuote: true,
+};
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles.module.scss'
 
@@ -10,28 +11,40 @@ export function MainPage() {
           <strong className={styles.sectionTitle}>Categories</strong>
           <div className={styles.categorySection}>
             <Link href="/product/" passHref>
-              <div style={{ backgroundImage: "url('/images/category/new.png')" }} className={styles.categorySectionContent}>
+              <div
+                style={{ backgroundImage: "url('/images/category/new.png')" }}
+                className={styles.categorySectionContent}
+              >
                 <span className={styles.categorySectionTitle}>
                   New
                 </span>
               </div>
             </Link>
             <Link href="/product/" passHref>
-              <div style={{ backgroundImage: "url('/images/category/tshirt.png')" }} className={styles.categorySectionContent}>
+              <div
+                style={{ backgroundImage: "url('/images/category/tshirt.png')" }}
+                className={styles.categorySectionContent}
+              >
                 <span className={styles.categorySectionTitle}>
                   t-Shirt
                 </span>
               </div>
             </Link>
             <Link href="/product/" passHref>
-              <div style={{ backgroundImage: "url('/images/category/jacket.png')" }} className={styles.categorySectionContent}>
+              <div
+                style={{ backgroundImage: "url('/images/category/jacket.png')" }}
+                className={styles.categorySectionContent}
+              >
                 <span className={styles.categorySectionTitle}>
                   Jacket
                 </span>
               </div>
             </Link>
             <Link href="/product/" passHref>
-              <div style={{ backgroundImage: "url('/images/category/pants.png')" }} className={styles.categorySectionContent}>
+              <div
+                style={{ backgroundImage: "url('/images/category/pants.png')" }}
+                className={styles.categorySectionContent}
+              >
                 <span className={styles.categorySectionTitle}>
                   Pants
                 </span>
@@ -41,21 +54,73 @@ export function MainPage() {
         </div>
 
         <div className={styles.trendingContainer}>
-          <strong>Trending collections</strong>
+          <strong className={styles.sectionTitle}>Trending collections</strong>
 
           <div className={styles.trendingSection}>
             <Link href="/product/" passHref>
-              <div style={{ background: "linear-gradient(37.44deg, #000000 2.71%, rgba(0, 0, 0, 0) 50.1%), no-repeat center/cover url('/images/category/jacket.png') , #171717" }} className={styles.trendingSectionContent}>
+              <div
+                style={{
+                  background: "linear-gradient(37.44deg, #000000 2.71%, rgba(0, 0, 0, 0) 50.1%), no-repeat center/cover url('/images/category/jacket.png') , #171717"
+                }}
+                className={styles.trendingSectionContent}
+                >
                 <span className={styles.trendingSectionTitle}>
-                  New
+                  New album collection
                 </span>
               </div>
             </Link>
             <Link href="/product/" passHref>
-              <div style={{ background: "linear-gradient(37.44deg, #000000 2.71%, rgba(0, 0, 0, 0) 50.1%), no-repeat center/cover url('/images/category/jacket.png') , #171717" }} className={styles.trendingSectionContent}>
+              <div
+                style={{
+                  background: "linear-gradient(37.44deg, #000000 2.71%, rgba(0, 0, 0, 0) 50.1%), no-repeat center/cover url('/images/category/jacket.png') , #171717"
+                  }}
+                className={styles.trendingSectionContent}
+              >
                 <span className={styles.trendingSectionTitle}>
-                  New
+                  Bring me a new Horizon  Collection album
                 </span>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <div className={styles.partnersContainer}>
+          <strong className={styles.sectionTitle}>Partners</strong>
+
+          <div className={styles.partnersSection}>
+            <Link href="/product/" passHref>
+              <div
+                className={styles.partnersSectionContent}
+              >
+                <Image src="/images/partners/spotify.png" layout='fill' alt="Spotify Music Partner"></Image>
+              </div>
+            </Link>
+            <Link href="/product/" passHref>
+              <div
+                className={styles.partnersSectionContent}
+              >
+                <Image src="/images/partners/deezer.png" layout='fill' alt="Deezer Music Partner"></Image>
+              </div>
+            </Link>
+            <Link href="/product/" passHref>
+              <div
+                className={styles.partnersSectionContent}
+              >
+                <Image src="/images/partners/googleMusic.png" layout='fill' alt='Google Music Partner'></Image>
+              </div>
+            </Link>
+            <Link href="/product/" passHref>
+              <div
+                className={styles.partnersSectionContent}
+              >
+                <Image src="/images/partners/shazam.png" layout='fill' alt='Shazam Music Partner'></Image>
+              </div>
+            </Link>
+            <Link href="/product/" passHref>
+              <div
+                className={styles.partnersSectionContent}
+              >
+                <Image src="/images/partners/appleMusic.png" layout='fill'  alt='Apple Music Partner'></Image>
               </div>
             </Link>
           </div>
